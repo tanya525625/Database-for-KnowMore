5fbc95c385f4882bebce2369c9001483205ff655
tanya525625 <tanya_postcrossing@mail.ru>
2018-11-18 20:28:54 +0300

    Test data are added

---
+++ Filling.sql
@@ -0,0 +1,17 @@
+use knowmoredb;
+
+#INSERT INTO user VALUES ('1','Zubakhina', 'Tatyana', 'Tanya7492401', '250', '12345','tanya_postcrossing@mail.ru'); 
+#INSERT INTO user VALUES ('2','Bainova', 'Ekaterina', 'Katunchik', '155', '123456','katya@mail.ru'); 
+#INSERT INTO user VALUES ('3','Mochilova', 'Tatyana', 'Tanushka', '350', '123','tanya@mail.ru'); #INSERT INTO question VALUES('1', '0', '0', 'What is 2 * 2?', '1', '2', '1', '1');
+#INSERT INTO interest_area_user VALUES ('1', 'math', '100', '1');
+#INSERT INTO interest_area_user VALUES ('2', 'literature', '80', '1');
+#INSERT INTO interest_area_user VALUES ('3', 'math','120', '2');
+#INSERT INTO interest_area_user VALUES ('4', 'programming', '55', '3');
+#INSERT INTO moderation VALUES('1', '1', '1');
+#INSERT INTO moderation VALUES ('2', '1', '3');
+#INSERT INTO question VALUES ('1', '0', '0', 'What is 2*2?', '1', '2', '1');
+#INSERT INTO answer VALUES ('1', '4', '0', '0', '1', '1', '1', '1');
+#interest_area_questiNSERT INTO interest_area_quest VALUES ('1', 'math', '1');
+
+
+
